08991e82e51f07dedfc343680de1cd83feddf4b7
m4rkbello <markbello10041999@gmail.com>
2025-05-25 18:51:15 +0800

    HOW TO USE VIEW COMPONENT

--- App.tsx
+++ App.tsx
@@ -10,15 +10,41 @@ import {
 function App(): React.JSX.Element {
 
 
-  return (
-    <View>
-      <Text>HELLO MARK</Text>
+  return <View style={styles.container}>
+    <View style={styles.boxContainer}>
+      <View style={styles.redBox} />
+      <View style={styles.blueBox} />
+      <View style={styles.greenBox} />
+      <View />
     </View>
-  );
+  </View>
+    ;
 }
 
 const styles = StyleSheet.create({
-
-}); 
+  container: {
+    marginBottom: 20,
+  },
+  boxContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  redBox: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+  },
+  blueBox: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'blue',
+  },
+  greenBox: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'green',
+  },
+});
 
 export default App;
