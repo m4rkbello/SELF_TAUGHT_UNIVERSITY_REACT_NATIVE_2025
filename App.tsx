a2b77a2fd5a862500cdcc6a6976bd6e3b7fdeccc
m4rkbello <markbello10041999@gmail.com>
2025-05-26 22:46:37 +0800

    ADD IMAGE, BUTTON COMPONENT WITH USESTATE

--- App.tsx
+++ App.tsx
@@ -1,29 +1,43 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Image,
+  Button
 } from 'react-native';
 
 function App(): React.JSX.Element {
 
 
+  const [count, setCount] = useState<number>(0)
+
+
   return (<View style={styles.container}>
+    {/**VIEW COMPONENT */}
     <View style={styles.boxContainer}>
       <View style={styles.redBox} />
       <View style={styles.blueBox} />
       <View style={styles.greenBox} />
       <View />
       {/**TEXT COMPONENT */}
-
-
     </View>
     <Text style={styles.myFirstText}>THIS IS MY FIRST APP
       <Text style={styles.nestedText}>MY NAME IS <Text style={styles.nestedTextBold}>MARK BELLO</Text>
       </Text>
     </Text>
+
+    {/**IMAGE COMPONENT WITH ONLINE LINK*/}
+    <Image style={styles.image} source={{ uri: 'https://picsum.photos/id/237/200/300' }} />
+
+    {/**IMAGE COMPONENT WITH LOCAL */}
+    <Image style={styles.imageLocal} source={require('./assets/pogiha.jpg')} />
+
+    {/**BUTTON COMPONENT */}
+    <Button title="Click Me" onPress={() => setCount(count + 1)} />
+    <Text> count is {count} </Text>
   </View>
   );
 }
@@ -60,6 +74,17 @@ const styles = StyleSheet.create({
   },
   nestedTextBold: {
     fontWeight: 'bold',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    marginBottom: 10
+  },
+  imageLocal: {
+    width: 150,
+    height: 150,
+    marginBottom: 10
   }
 
 });
